09c6100ae7d0f60c6dca2bfc4ce848c91b440c61
wuyifan <wyf000219@gmail.com>
2023-01-06 23:50:36 +0800

    编译出能返回指定数值的程序

---
+++ test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+assert() {
+    input="$1"
+    expect="$2"
+
+    ./rvcc "$1" > tmp.s || exit
+
+    "$RISCV"/bin/riscv64-unknown-linux-gnu-gcc -static -o tmp tmp.s
+    
+    "$RISCV"/bin/qemu-riscv64 -L "$RISCV"/sysroot ./tmp
+
+    actual="$?"
+
+    if [ "$actual" = "$expect" ]; then
+        echo "$input => $actual"
+    else
+        echo "$input => $expect expected, but got $actual"
+        exit 1
+    fi
+}
+
+
+assert 1 1
+assert 42 42
+
+echo OK
